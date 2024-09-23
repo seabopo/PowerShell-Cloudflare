9dccb569e9572ec56e11a240e9e9fa67cfd145cf
seabopo <seabopo@icloud.com>
2024-09-23 01:45:25 -0500

    update gitignore

--- tests/init-tokens.ps1
+++
@@ -1,5 +0,0 @@
-
-$env:CLOUDFLARE_ACCOUNT_ID = 'deecf636e268a993587cabed31567a07'
-$env:CLOUDFLARE_AUTH_TOKEN = ConvertTo-SecureString -String 'b78SMHOaxvwWqqw8Qpb-wUhJh4L24Pz_nFUVEuI8' -AsPlainText -Force |
-                             ConvertFrom-SecureString
-
